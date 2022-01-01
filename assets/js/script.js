184506da2b302598648010877c06a41a1aa7effb
Kawilder <kevin_wilde564@yahoo.com>
2022-01-01 14:29:56 -0600

    added bootstrap elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -141,6 +141,37 @@ $(".list-group").on("click", "span", function() {
   dateInput.trigger("focus");
 });
 
+// value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function() {
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  
+  //get the task's positon in the list of other li elements 
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  //update task in arrat and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  //recreate span element with bootstrap classes
+  var raskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
+});
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
